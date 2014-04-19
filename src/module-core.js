515a5f2ff1e74be6c79cf323f1ad56dfe349932d
Jonnie Spratley <jonniespratley@me.com>
2014-04-19 14:36:54 -0700

    added updated

--- src/module-core.js
+++ src/module-core.js
@@ -13,40 +13,63 @@ function core(options) {
 	//Invoke the EventEmitter
 	events.EventEmitter.call(this);
 
-	//this.options = utils.inherits(options, this);
 	this.options = options;
 	this.name = 'core module';
+	this.extension = this.options.extension ? this.options.extension : {};
 
 	//Initialize the module
 	this.init = function () {
 		this.emit('init', this);
 		utils.log('core.init()', this.options);
-	}
-
-	//Other public methods
-	this.create = function(args, callback){
-
-	};
-	this.read = function(args, callback){
-
-	};
-	this.update = function(args, callback){
-
-	};
-	this.destroy = function(args, callback){
-
 	};
 
+	//Call method which will call method on extension if there
+	this.call = function(){
+		//Log
+		utils.log(this.extension.hasOwnProperty([arguments[0]]), arguments[0], arguments);
 
-	//Extension methods
+		//emit
+		this.emit(arguments[0], arguments);
+
+		//check
+		if(this.extension.hasOwnProperty([arguments[0]])){
+
+			//invoke
+			this.extension[arguments[0]].call(this.extension[arguments[1]])
+		} else {
+
+			//error
+			throw Error('You need to provide an extension!');
+		}
+	};
 
 
+	this.save = function (args) {
+
+		this.call('save', args);
+
+	};
+
+	this.read = function (args) {
+		utils.log('read', args);
+		this.emit('read', args);
+	};
+
+	this.update = function (args) {
+		utils.log('update', args);
+		this.emit('update', args);
+	};
+
+	this.destroy = function (args) {
+		utils.log('destroy', args);
+		this.emit('destroy', args);
+	};
 
 };
 
-
 //Make core inherit all of EventEmitter properties/methods
 utils.inherits(core, events.EventEmitter);
+
 
 console.log(module.id);
 
--- src/module-extension.js
+++ src/module-extension.js
@@ -3,6 +3,9 @@ var extension = {
 	name: 'module extension',
 	init: function(){
 		console.log('extension.init()');
+	},
+	save: function(args){
+		console.log('extension.save');
 	}
 };
 
